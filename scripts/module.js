8c919d38b8366d2af9e5c337ad2ab490e137422d
Whastic A <nanourora@gmail.com>
2023-11-07 20:12:22 +0530

    reset

--- scripts/module.js
+++ scripts/module.js
@@ -1,33 +1,7 @@
-let socket;
-
-Hooks.once("socketlib.ready", () => {
-	socket = socketlib.registerModule("my-module");
-	socket.register("hello", showHelloMessage);
-	socket.register("add", add);
-});
-
 Hooks.once('init', async function() {
-    console.log('2t2f initialised');
+
 });
 
-Hooks.once("ready", async () => {
-	// Let's send a greeting to all other connected users.
-	// Functions can either be called by their given name...
-	socket.executeForEveryone("hello", game.user.name);
-	// ...or by passing in the function that you'd like to call.
-	socket.executeForEveryone(showHelloMessage, game.user.name);
-	// The following function will be executed on a GM client.
-	// The return value will be sent back to us.
-	const result = await socket.executeAsGM("add", 5, 3);
-	console.log(`The GM client calculated: ${result}`);
+Hooks.once('ready', async function() {
+
 });
-
-function showHelloMessage(userName) {
-	console.log(`User ${userName} says hello!`);
-}
-
-function add(a, b) {
-	console.log("The addition is performed on a GM client.");
-	return a + b;
-}
-
